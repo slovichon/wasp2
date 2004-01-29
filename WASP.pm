4bfb65761be165b81e66846937bead20ddda79d7
jaredy <>
2004-01-29 00:55:59 +0000

    doc fmt

--- WASP.pm
+++ WASP.pm
@@ -30,27 +30,28 @@ WASP - Web Application Structure for Perl programs
 
 =head1 DESCRIPTION
 
-WASP is a means to provide structure for Web applications.  The core module
-mostly provides error-handling routines, but is intended, if all components
-of a Web application system use it, to provide a consistent set of behaviors
-which are all robust, are easy to set up and execute, and are clean, concise,
-and predictable.
+WASP is a means to provide structure for Web applications.
+The core module mostly provides error-handling routines, but is intended,
+if all components of a Web application system use it, to provide a consistent
+set of behaviors which are all robust, are easy to set up and execute,
+and are clean, concise, and predictable.
 
 =head2 Administrators
 
 Administrators of WASP-obeying Web applications may wish to refine the behavior
 of WASP actions, so that when something fails in the application, the administrator
 may rest at ease knowing the application will behave as he or she has configured it
-to do so.  In this case, an administrator will only care about the behaviorial
-methods and perhaps the accessor methods, to set and retrieve the behaviorial
-semantics of WASP.
+to do so.
+In this case, an administrator will only care about the behaviorial methods and
+perhaps the accessor methods, to set and retrieve the behaviorial semantics of WASP.
 
 =head2 Programmers
 
 Programmers using WASP, most likely for adding consistency in his or her Web
-application, will only need to know of the core action methods.  The behaviorial
-and accessor methods are only for refining what exactly happens when a core action
-is to be executed and a programmer need not know these exact details.
+application, will only need to know of the core action methods.
+The behaviorial and accessor methods are only for refining what exactly happens
+when a core action is to be executed and a programmer need not know these exact
+details.
 
 =cut
 package WASP;
@@ -83,8 +84,9 @@ sub new {
 
 This behaviorial method affects the invocation of Perl's C<die> on error.
 If no value is given, calling this method will turn the calling of Perl's
-C<die> on.  Otherwise, the value of the Boolean argument will determine
-whether or not to invoke Perl's C<die> on error.
+C<die> on.
+Otherwise, the value of the Boolean argument will determine whether or not
+to invoke Perl's C<die> on error.
 
 =cut
 sub die_on_errors {
@@ -104,9 +106,11 @@ sub will_die_on_errors {
 =item $wasp-E<gt>display_errors([$bool]);
 
 This behaviorial method affects the printing of error messages to standard
-output on error.  In no value is given, calling this method will turn the
-displaying of error messages on.  Otherwise, the value of the Boolean argument
-will determine whether or not to print error messages on error.
+output on error.
+In no value is given, calling this method will turn the displaying of error
+messages on.
+Otherwise, the value of the Boolean argument will determine whether or not
+to print error messages on error.
 
 =cut
 sub display_errors {
@@ -125,11 +129,12 @@ sub will_display_errors {
 
 =item $wasp-E<gt>log_errors($file);
 
-This behaviorial method affects the logging of error messages on error.  The
-argued file will attempt to be opened in append mode and an error message, in
-the form below, will attempt to be placed at the end of the log file.
+This behaviorial method affects the logging of error messages on error.
+The argued file will attempt to be opened in append mode and an
+error message, in the form below, will attempt to be placed at the end
+of the log file.
 
-C<Cannot do something; detail1: description1; detail2: description2; detailN: ...>
+S<Cannot do something; detail1: description1; detail2: description2; detailN: ...>
 
 Note that if an error arises and the error log file cannot be written to, logging
 will simply be ignored (as opposed to, say an infinite loop of handling an
@@ -148,8 +153,8 @@ sub log_errors {
 This accessor method is used to determine if error messages will attempt to be
 logged if an error is raised.
 
-Perhaps this method should be named "will_try_to_log." But it isn't, for hopefully
-obvious reasons.
+Perhaps this method should be named E<quot>will_try_to_log.E<quot>
+But it isn't, for hopefully obvious reasons.
 
 =cut
 sub will_log {
@@ -169,9 +174,9 @@ sub error_log {
 =item $wasp-E<gt>handle_errors($sub);
 
 This behaviorial method affects the delegation of error-handling to other
-user-defined routines on error.  An anonymous subroutine or subroutine
-reference may be given as an argument whose instructions will be processed
-on error.
+user-defined routines on error.
+An anonymous subroutine or subroutine reference may be given as an argument
+whose instructions will be processed on error.
 
 =cut
 sub handle_errors {
@@ -180,8 +185,8 @@ sub handle_errors {
 
 =item $wasp-E<gt>will_handle;
 
-This accessor method is used to determine whether or not a delegated error-handling
-routine is to be used if an error is raised.
+This accessor method is used to determine whether or not a delegated
+error-handling routine is to be used if an error is raised.
 
 =cut
 sub will_handle {
@@ -190,9 +195,10 @@ sub will_handle {
 
 =item $wasp-E<gt>error_handler;
 
-This accessor method is used to determine the delegated error-handling routine that
-will be invoked if an error is raised.  It will return either a reference to an
-anonymous or named subroutine, or C<undef>, if no such delegate ever specified.
+This accessor method is used to determine the delegated error-handling routine
+that will be invoked if an error is raised.  It will return either a reference
+to an anonymous or named subroutine, or C<undef>, if no such delegate ever
+specified.
 
 =cut
 sub error_handler {
@@ -202,8 +208,9 @@ sub error_handler {
 =item $wasp-E<gt>has_module($mod);
 
 This accessor method can be used to determine if the given Perl module exists and
-can be loaded.  It will not load the module, but instead return the full path to the
-module in question if it can be found in the include path or an C<undef> value.
+can be loaded.
+It will not load the module, but instead return the full path to the module in
+question if it can be found in the include path or an C<undef> value.
 
 =cut
 sub has_module {
@@ -217,18 +224,21 @@ sub has_module {
 
 =item $wasp-E<gt>throw($errmsg);
 
-This action method performs an exception raise.  The actual semantics of what will happen
-can be heavily modified by the available error-handling behaviorial methods.  By default
-C<throw> will try to append to the given error message information such as the operating
-system error message (e.g., as the result of failed I/O or a filesystem problem).  It will
-also attempt to trace back through all subroutine calls to find the origin of the problem
-(in most cases going too far to provide useful information) and when the error report was
-generated.
+This action method performs an exception raise.
+The actual semantics of what will happen can be heavily modified by the available
+error-handling behaviorial methods.
+By default C<throw> will try to append to the given error message information such
+as the operating system error message (e.g., as the result of failed I/O or a
+filesystem problem).
+It will also attempt to trace back through all subroutine calls to find the origin
+of the problem (in most cases going too far to provide useful information) and when
+the error report was generated.
 
 If logging is enabled (see C<log_errors>), the error message will then attempt to be
-written to the log file.  If the logging request cannot be fulfilled, additional error
-information related to the failed logging attempt is appended to the error message and
-further logging action is ignored.
+written to the log file.
+If the logging request cannot be fulfilled, additional error information related to
+the failed logging attempt is appended to the error message and further logging
+action is ignored.
 
 Next, errors are displayed to standard output (see C<display_errors>) if configuration
 specifies that they should.
