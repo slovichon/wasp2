80bc60739d296ea39cfa45aeacd18ee1a157b2d5
yanovich <>
2014-09-02 01:48:23 +0000

    flesh out real contents here

--- OOF/Element/List.pm
+++ OOF/Element/List.pm
@@ -1,15 +1,32 @@
 # $Id$
 
-sub list {
-	my ($this, $type, @data) = @_;
-	my $out = $this->list_start($type);
-	$out .= $this->list_item($_) foreach @data;
-	$out .= $this->list_end($type);
-	return $out;
+package OOF::Element::List;
+
+use OOF::Element;
+use strict;
+use warnings;
+
+our $VERSION = 0.1;
+our @ISA = qw(OOF::Element);
+
+sub new {
+	my $pkg    = shift;
+	my $filter = shift;
+
+	my $prefs = ref $_[0] eq "HASH" ? shift : { };
+	my $type;
+	if (%$prefs) {
+		$type = $prefs->{type};
+		delete $prefs->{type};
+	} else {
+		$type = shift;
+	}
+
+	my $this = $pkg->SUPER::new($filter, $prefs, "");
+	$this->{type} = $type;
+	$this->{items} = [ @_ ];
+
+	return $this;
 }
 
-sub list_start;
-sub list_end;
-sub list_item;
-
-0;
+1;
